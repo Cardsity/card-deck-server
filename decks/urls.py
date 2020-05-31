2f0f1b04f4b715c09941b21529c45353d3315661
ravi0li <moritz@howtoflex.club>
2020-05-31 19:31:45 +0200

    feat(decks): Added DeckListView

--- decks/urls.py
+++ decks/urls.py
@@ -6,4 +6,5 @@ app_name = 'decks'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('deck/<int:pk>/json/', views.DeckJSONView.as_view(), name='deck_as_json'),
+    path('deck/list/', views.DeckListView.as_view(), name='deck_list'),
 ]
--- decks/views.py
+++ decks/views.py
@@ -1,12 +1,20 @@
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.list import ListView
 from .models import Deck
 
 
 class IndexView(View):
     def get(self, request):
         return HttpResponse("Hello world!")
+
+
+class DeckListView(ListView):
+    """Shows all decks."""
+    model = Deck
+    template_name = 'decks/list.html'
+    paginate_by = 20
 
 
 class DeckJSONView(SingleObjectMixin, View):
