6315bcfed7d85cf1248eea5cfaedacff868b0dbe
ravi0li <moritz@howtoflex.club>
2020-05-31 15:54:05 +0200

    feat(decks): Added models

--- decks/admin.py
+++ decks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Deck, BlackCard, WhiteCard
 
-# Register your models here.
+
+class BlackCardInline(admin.TabularInline):
+    """Tabular inline for black cards."""
+    model = BlackCard
+    fk_name = "deck"
+
+
+class WhiteCardInline(admin.TabularInline):
+    """Tabular inline for white cards."""
+    model = WhiteCard
+    fk_name = "deck"
+
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    """The deck admin site."""
+    list_display = ('pk', 'name')
+    inlines = (BlackCardInline, WhiteCardInline)
--- decks/models.py
+++ decks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Deck(models.Model):
+    """A deck."""
+    name = models.CharField("Name", max_length=100)
+
+    class Meta:
+        # Order by desc primary key
+        ordering = ["-pk"]
+
+
+class BlackCard(models.Model):
+    """A black card which belongs to a deck."""
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    text = models.TextField("Text")
+    blanks = models.PositiveIntegerField("Blank count")
+
+
+class WhiteCard(models.Model):
+    """A white card which belongs to a deck."""
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    text = models.TextField("Text")
