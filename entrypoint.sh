939612af66aaf2c2f5a89927eb2c346f9c1d363d
ravi0li <moritz@howtoflex.club>
2020-06-05 16:43:41 +0200

    feat(docker): Migrate before creating superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Automatic migrate
+if [ -n "$DJANGO_AUTOMATIC_MIGRATE" ] ; then
+    python manage.py migrate --settings carddeckserver.settings.prod
+fi
+
 # Create superuser if parameters were supplied
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     python manage.py createsuperuser --settings carddeckserver.settings.prod --no-input
@@ -9,11 +14,6 @@ fi
 if [ -n "$DJANGO_COLLECTSTATIC" ] ; then
     python manage.py collectstatic --settings carddeckserver.settings.prod --noinput
     chown -R www-data:www-data /app/static/
-fi
-
-# Automatic migrate
-if [ -n "$DJANGO_AUTOMATIC_MIGRATE" ] ; then
-    python manage.py migrate --settings carddeckserver.settings.prod
 fi
 
 # Run the server
