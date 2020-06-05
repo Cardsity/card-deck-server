b52fcfbf26c7bf598749109ed60d3388ec27b740
ravi0li <moritz@howtoflex.club>
2020-06-05 16:32:30 +0200

    feat(docker): Fixes for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 # Create superuser if parameters were supplied
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    python manage.py createsuperuser --no-input
+    python manage.py createsuperuser --settings carddeckserver.settings.prod --no-input
 fi
 
 # Collect static files
 if [ -n "$DJANGO_COLLECTSTATIC" ] ; then
     python manage.py collectstatic --settings carddeckserver.settings.prod --noinput
+    chown -R www-data:www-data /app/static/
 fi
 
 # Automatic migrate
