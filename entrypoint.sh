70eb80d860b648fb5977e00a67e52bb2eb613a03
ravi0li <moritz@howtoflex.club>
2020-06-17 16:59:20 +0200

    fix(docker entrypoint): Sleep/Set DATABASE_URL before first database connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+# Sleep if sleep parameter is supplied
+if [ -n "$SLEEP" ] ; then
+    sleep "$SLEEP"
+fi
+
+# Set the DATABASE_URL environment variable if all mysql parameters are set
+if [ -n "$MYSQL_USERNAME" ] && [ -n "$MYSQL_PASSWORD" ] && [ -n "$MYSQL_DATABASE" ] && [ -n "$MYSQL_HOST" ] && [ -z "$DATABASE_URL" ] ; then
+    export DATABASE_URL="mysql://${MYSQL_USERNAME}:${MYSQL_PASSWORD}@$MYSQL_HOST/${MYSQL_DATABASE}"
+fi
+
 # Automatic migrate
 if [ -n "$DJANGO_AUTOMATIC_MIGRATE" ] ; then
     python manage.py migrate --settings carddeckserver.settings.prod
@@ -13,16 +23,6 @@ fi
 # Collect static files
 if [ -n "$DJANGO_COLLECTSTATIC" ] ; then
     python manage.py collectstatic --settings carddeckserver.settings.prod --noinput
-fi
-
-# Sleep if sleep parameter is supplied
-if [ -n "$SLEEP" ] ; then
-    sleep "$SLEEP"
-fi
-
-# Set the DATABASE_URL environment variable if all mysql parameters are set
-if [ -n "$MYSQL_USERNAME" ] && [ -n "$MYSQL_PASSWORD" ] && [ -n "$MYSQL_DATABASE" ] && [ -n "$MYSQL_HOST" ] && [ -z "$DATABASE_URL" ] ; then
-    export DATABASE_URL="mysql://${MYSQL_USERNAME}:${MYSQL_PASSWORD}@$MYSQL_HOST/${MYSQL_DATABASE}"
 fi
 
 # Run the server
